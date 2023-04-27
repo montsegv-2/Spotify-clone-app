8a34c58be08784242852fdd04567b0affcee3c7d
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-04-27 10:59:28 -0600

    Player component created

--- src/App.js
+++ src/App.js
@@ -2,34 +2,35 @@ import React, { useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login'
 import { getTokenFromResponse } from './spotify';
+import SpotifyWepApi from "spotify-web-api-js"
+import Player from './Player';
+
+const spotify = new SpotifyWepApi();
 
 function App() {
   const [token, setToken] = useState(null);
 
   //Run code based on a given condition  
   useEffect(() => {
-    const hash = getTokenFromResponse();  
+    const hash = getTokenFromResponse();
     window.location.hash = "";
     const _token = hash.acces_token;
 
     if(_token){
-      setToken(_token); 
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then( (user) => {
+        console.log(':person:',user);
+      })
     }
 
     console.log('I HAVE A TOKEN ', token)
   },[]);
 
   return (
-    <div className="App">
-      {
-        token ? (
-          <h1>I am Logged in </h1>
-          ) : (
-          <Login/>
-        )
-      }
-      
-    </div>
+    <div className="App"> { token ?  <Player/>  :  <Login/> } </div>
   );
 }
 
--- src/spotify.js
+++ src/spotify.js
@@ -1,10 +1,10 @@
 //https://developer.spotify.com/
 
-export const authEndpoint = 'htpps://accounts.spotify.com/authorize';
+export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
-const redirectUri = 'http://localhost:3000/';
 const clientId = 'd72e39aca2a64d129cacfae943d27c44';
-const scope = [
+const redirectUri = "http://localhost:3000/";
+const scopes = [
     "user-read-currently-playing",
     "user-read-recently-played",
     "user-read-playback-state",
@@ -25,6 +25,6 @@ export const getTokenFromResponse = () => {
     }, {});
 }
 
-export const acessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join(
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
     "%20"
-)}&response_type=token&show_dialog=true`;
+)}&response_type=token&show_dialog=true`;   
